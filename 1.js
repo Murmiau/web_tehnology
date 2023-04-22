d8f97dd2dbed76d4995712ad8efcd029c356ea9e
Vera Mirgorodscaya <mirgorodscya@mail.ru>
2023-04-22 03:13:04 +0300

    lesson3

---
+++ 1.js
@@ -0,0 +1,3 @@
+let degreeC = parseInt(prompt("Введите температуру в градусах цельсия: "));
+let degreeF = (9/5) * degreeC + 32;
+alert("Градусы цельсия: " + degreeC + "; Градусы фаренгейта: " + degreeF.toFixed(1)); 
---
+++ 2.js
@@ -0,0 +1,5 @@
+function greeting(name){
+    return("Привет,"+ name +", рады встрече!");
+}
+let name = prompt("Добро пожаловать! Введите свое имя: ");
+console.log(greeting(name));
